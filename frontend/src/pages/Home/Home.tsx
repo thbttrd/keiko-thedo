d25ce97e93e5ee08075537187a574f8a1f89c139
“Thibaut <thibaut.troude@theodo.com>
2024-10-03 17:42:22 +0200

    insert pokemon in homepage via list of components

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,14 +1,31 @@
 import styles from "./Home.module.css"
 import { Pokemon } from "components/Pokemon"
 
+const pokemonList = [
+  {
+    name: "Carapuce",
+    id: 7,
+  },
+  {
+    name: "Carabaffe",
+    id: 8,
+  },
+  {
+    name: "Tortank",
+    id: 9,
+  },
+]
+
+const pokemonElt = pokemonList.map(pokemon => {
+  return <Pokemon name={pokemon.name} id={pokemon.id} key={pokemon.id} />
+})
+
 export const Home = () => {
   return (
     <div className={styles.intro}>
       <div>Bienvenue sur ton futur pokédex !</div>
       <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
-      <Pokemon name="Carapuce" id={7} />
-      <Pokemon name="Carabaffe" id={8} />
-      <Pokemon name="Tortank" id={9} />
+      {pokemonElt}
     </div>
   )
 }
