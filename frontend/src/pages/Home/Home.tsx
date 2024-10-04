d3caf63ebb9c441c19ecabf7688d71349fa54e71
“Thibaut <thibaut.troude@theodo.com>
2024-10-04 14:11:27 +0200

    add a input and logic to filter Pokemon display

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import styles from "./Home.module.css"
 import { Pokemon } from "components/Pokemon"
+import React from "react"
 
 const pokemonList = [
   {
@@ -16,16 +17,37 @@ const pokemonList = [
   },
 ]
 
-const pokemonElt = pokemonList.map(pokemon => {
-  return <Pokemon name={pokemon.name} id={pokemon.id} key={pokemon.id} />
-})
-
 export const Home = () => {
+  const [filterValue, setFilterValue] = React.useState("")
+
+  function filterPokemonsByName(pokemons: Pokemon[], name: string): Pokemon[] {
+    return pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(name.toLowerCase()))
+  }
+
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterValue(event.target.value)
+  }
+
+  interface Pokemon {
+    name: string
+    id: number
+  }
+
   return (
     <div className={styles.intro}>
       <div>Bienvenue sur ton futur pokédex !</div>
       <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
-      {pokemonElt}
+      <label htmlFor="pokemonFilter"> Choisis ton pokemon</label>
+      <input
+        id="pokemonFilter"
+        className={styles.input}
+        onChange={onInputChange}
+        placeholder="Id du Pokemon"
+        value={filterValue}
+      />
+      {filterPokemonsByName(pokemonList, filterValue).map(pokemon => {
+        return <Pokemon name={pokemon.name} id={pokemon.id} key={pokemon.id} />
+      })}
     </div>
   )
 }
