a627d316528475ad02105d531eaedf757ff761dd
“Thibaut <thibaut.troude@theodo.com>
2024-10-14 11:10:50 +0200

    implement msw version fix

--- frontend/src/pages/Home/Home.test.tsx
+++ frontend/src/pages/Home/Home.test.tsx
@@ -1,18 +1,14 @@
 import { render, screen } from "@testing-library/react"
 import { Home } from "./index"
-import { rest } from "msw"
+import { http, HttpResponse } from "msw"
 import { setupServer } from "msw/node"
 
 const server = setupServer(
-  rest.get("http://localhost:8000/pokemon", (req, res, ctx) => {
-    // Respond with a mocked user token that gets persisted
-    // in the `sessionStorage` by the `Login` component.
-    return res(
-      ctx.json([
-        { id: 1, name: "bulbasaur", height: 7, weight: 69 },
-        { id: 2, name: "ivysaur", height: 10, weight: 130 },
-      ]),
-    )
+  http.get("http://localhost:8000/pokemon", () => {
+    return HttpResponse.json([
+      { id: 1, name: "bulbasaur", height: 7, weight: 69 },
+      { id: 2, name: "ivysaur", height: 10, weight: 130 },
+    ])
   }),
 )
 
